c7992ce73f0b7fbf1b99685e8fbac8677d237146
Rayen Mhamdi <rayen.mhamdi@abaut.de>
2025-09-26 15:43:12 +0100

    add admin content

--- team/admin.py
+++ team/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Season, Category, Player, MatchParticipation, Match
+
+admin.site.register(Season)
+admin.site.register(Category)
+admin.site.register(Player)
+admin.site.register(MatchParticipation)
+admin.site.register(Match)
